667d28641a7d71e61b861cc33a79432543fa1ae5
Nazmul Hasan Apu <nazmul.apu.hasan@gmail.com>
2018-02-23 23:25:01 +0200

    cache busting feature added

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,37 +1,59 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import WebpackMd5Hash from 'webpack-md5-hash';
+//import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   //debug: true,
   devtool: 'source-map',
   //noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
     // Create HTML file that includes reference to bundled JS.
+   // new ExtractTextPlugin('[name].[contenthash].css'),
+    new WebpackMd5Hash(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     new webpack.optimize.DedupePlugin(), 
     new webpack.optimize.UglifyJsPlugin(),
     
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
       inject: true
     })
   ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: [
+      {test: /\.css$/, loaders:  [
         require.resolve('style-loader'),
         require.resolve('css-loader')
-      ]}
+      ]},
+
+      //{test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
